b7b2e9d403a910298db187f4789f321328d2f031
TaeyoungNoah <tyty09171007@gmail.com>
2022-01-19 14:44:40 +0900

    Chapter 3

---
+++ src/main/java/practice/book/webservice/service/posts/PostsService.java
@@ -0,0 +1,39 @@
+package practice.book.webservice.service.posts;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import practice.book.webservice.domain.posts.Posts;
+import practice.book.webservice.domain.posts.PostsRepository;
+import practice.book.webservice.web.dto.PostsResponseDto;
+import practice.book.webservice.web.dto.PostsUpdateRequestDto;
+import practice.book.webservice.web.dto.PostsSaveRequestDto;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+}
---
+++ src/main/java/practice/book/webservice/web/PostsApiController.java
@@ -0,0 +1,33 @@
+package practice.book.webservice.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import practice.book.webservice.service.posts.PostsService;
+import practice.book.webservice.web.dto.PostsResponseDto;
+import practice.book.webservice.web.dto.PostsUpdateRequestDto;
+import practice.book.webservice.web.dto.PostsSaveRequestDto;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update (@PathVariable Long id,
+                        @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+
+
+}
